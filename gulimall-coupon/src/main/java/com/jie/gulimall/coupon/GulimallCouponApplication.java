626f95bf1259401adac707db5f9b7ceed1c24e04
sujieee <13156660820@163.com>
2020-12-14 22:39:53 +0800

    项目初始化结构

---
+++ gulimall-coupon/src/main/java/com/jie/gulimall/coupon/GulimallCouponApplication.java
@@ -0,0 +1,13 @@
+package com.jie.gulimall.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallCouponApplication.class, args);
+    }
+
+}
---
+++ gulimall-coupon/src/test/java/com/jie/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jie.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
